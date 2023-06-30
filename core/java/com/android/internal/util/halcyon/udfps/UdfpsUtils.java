5b584427ae9ef72277cc7081533e2eddeab31524
Chris Crump <chris@cypheros.co>
2023-06-30 01:26:17 +0000

    core: Utils: Add method to check udfps support
    Signed-off-by: HeroBuxx <me@herobuxx.me>

---
+++ core/java/com/android/internal/util/halcyon/udfps/UdfpsUtils.java
@@ -0,0 +1,14 @@
+package com.android.internal.util.halcyon.udfps;
+
+import android.content.Context;
+
+import com.android.internal.util.ArrayUtils;
+
+public class UdfpsUtils {
+    public static boolean hasUdfpsSupport(Context context) {
+        int[] udfpsProps = context.getResources().getIntArray(
+                com.android.internal.R.array.config_udfps_sensor_props);
+
+        return !ArrayUtils.isEmpty(udfpsProps);
+    }
+}
